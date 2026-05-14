c6339f50cdd50363cbc527b488163996f9eac6bd
Amlal El Mahrouss <amlal@nekernel.org>
2026-05-14 06:39:32 +0200

    [FEAT] examples: add `enable_if` test case for #96.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- test/distance_test.cpp
+++ test/distance_test.cpp
@@ -1,4 +1,5 @@
 // Copyright (C) 2017 Michel Morin.
+// Copyright (C) 2026 Amlal El Mahrouss.
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
@@ -10,13 +11,29 @@
 #include <boost/core/lightweight_test.hpp>
 #include <boost/iterator/distance.hpp>
 #include <boost/iterator/transform_iterator.hpp>
+#include <boost/iterator/is_iterator.hpp>
 
 int twice(int x) { return x + x; }
+
+bool truetor(bool x) { return true; }
+#define BOOST_NIL_TRUETOR (false)
 
 template <typename Iterator>
 void test_distance(Iterator it_from, Iterator it_to, int n)
 {
     BOOST_TEST(boost::distance(it_from, it_to) == n);
+}
+
+template <typename Iterator>
+void test_constrain_distance(Iterator it_from, Iterator it_to, int n, bool expected)
+{
+    BOOST_TEST(boost::is_iterator<decltype(it_from)>::value == expected);
+    BOOST_TEST(boost::is_iterator<decltype(it_to)>::value == expected);
+
+    if (boost::is_iterator<decltype(it_from)>::value && n) {
+      boost::container::slist<int> slist(it_from, it_to);
+      test_distance(slist.begin(), slist.end(), n);
+    }
 }
 
 int main()
@@ -78,6 +95,22 @@ int main()
           , boost::make_transform_iterator(ints.end(), twice)
           , 3
         );
+    }
+
+    {
+      boost::container::slist<int> ints(ptr1, ptr2);
+
+      bool expected = true;
+
+      test_constrain_distance(ints.begin(), ints.end(), 3, expected);
+
+      test_constrain_distance(
+        boost::make_transform_iterator(ints.begin(), truetor),
+      boost::make_transform_iterator(ints.end(), truetor), 3, truetor(BOOST_NIL_TRUETOR));
+
+      test_constrain_distance(&ptr1, &ptr2, 3, !expected);
+      
+      test_constrain_distance(nullptr, nullptr, 0, !expected);
     }
 
     return boost::report_errors();
