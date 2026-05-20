9a017ec4d5937204ac6041af575eba06c4c76d34
Amlal El Mahrouss <amlal@nekernel.org>
2026-05-20 08:49:11 +0200

    [CHORE] src: Removed the unused iterator_traits include.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- test/distance_test.cpp
+++ test/distance_test.cpp
@@ -5,7 +5,6 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/iterator/iterator_traits.hpp>
 #include <cstddef>
 #include <vector>
 #include <list>
