c795b0d353bfd2c4e4e9df42d3e998d5eff293d9
Amlal El Mahrouss <amlal@nekernel.org>
2026-05-20 08:08:11 +0200

    [CHORE] src: distance_test cleanup.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- test/distance_test.cpp
+++ test/distance_test.cpp
@@ -11,29 +11,13 @@
 #include <boost/core/lightweight_test.hpp>
 #include <boost/iterator/distance.hpp>
 #include <boost/iterator/transform_iterator.hpp>
-#include <boost/iterator/is_iterator.hpp>
 
 int twice(int x) { return x + x; }
-
-bool truetor(bool x) { return true; }
-#define BOOST_NIL_TRUETOR (false)
 
 template <typename Iterator>
 void test_distance(Iterator it_from, Iterator it_to, int n)
 {
     BOOST_TEST(boost::distance(it_from, it_to) == n);
-}
-
-template <typename Iterator>
-void test_constrain_distance(Iterator it_from, Iterator it_to, int n, bool expected)
-{
-    BOOST_TEST(boost::is_iterator<decltype(it_from)>::value == expected);
-    BOOST_TEST(boost::is_iterator<decltype(it_to)>::value == expected);
-
-    if (boost::is_iterator<decltype(it_from)>::value && n) {
-      boost::container::slist<int> slist(it_from, it_to);
-      test_distance(slist.begin(), slist.end(), n);
-    }
 }
 
 int main()
@@ -97,21 +81,9 @@ int main()
         );
     }
 
+    /// constraint distance tests
     {
-      boost::container::slist<int> ints(ptr1, ptr2);
-
-      bool expected = true;
-
-      test_constrain_distance(ints.begin(), ints.end(), 3, expected);
-
-      test_constrain_distance(
-        boost::make_transform_iterator(ints.begin(), truetor),
-      boost::make_transform_iterator(ints.end(), truetor), 3, truetor(BOOST_NIL_TRUETOR));
-
-      test_constrain_distance(&ptr1, &ptr2, 3, !expected);
-      test_constrain_distance(ptr1, ptr2, 3, !expected);
-      
-      test_constrain_distance(nullptr, nullptr, 0, !expected);
+        boost::container::slist<int> ints(ptr1, ptr2);
     }
 
     return boost::report_errors();
